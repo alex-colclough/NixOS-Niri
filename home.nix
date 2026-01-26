08ab58bcb5ff26400fc90d8e6c7448b4731b9280
Alex Colclough <alex.colclough@protonmail.com>
2026-01-26 13:23:50 -0500

    adding changes

--- home.nix
+++ home.nix
@@ -7,20 +7,11 @@
 	
 	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			btw = "echo i use niri btw";
-		};
-		profileExtra = ''
-			if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-				 niri-session
-			fi
-		'';
-	};
-        home.file.".config/waybar" .source = ./config/waybar;
 
-	home.packages = [ pkgs.bibata-cursors ];
+	home.packages = with pkgs; [
+	  bibata-cursors
+	];
+
 	home.pointerCursor = {
 		gtk.enable = true;
 		package = pkgs.bibata-cursors;
@@ -28,10 +19,10 @@
 		size = 20;
 	};
 
-
-
-
-
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			btw = "echo i use niri btw";
+		};
+	};
 }
-
-
